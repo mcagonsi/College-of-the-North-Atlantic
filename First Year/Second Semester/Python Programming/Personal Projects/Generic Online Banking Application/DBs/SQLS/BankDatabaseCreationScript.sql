695f63d2bbb60d1d8376b1fcdff0a374e7321f34
MichaelAgonsi <144907804+MichaelAgonsi@users.noreply.github.com>
2024-05-02 17:00:10 -0230

    Banking Software Project Database Connection, Creation and Function Sucess 1.0

--- First Year/Second Semester/Python Programming/Personal Projects/Generic Online Banking Application/DBs/SQLS/BankDatabaseCreationScript.sql
+++ First Year/Second Semester/Python Programming/Personal Projects/Generic Online Banking Application/DBs/SQLS/BankDatabaseCreationScript.sql
@@ -1,12 +1,17 @@
+DROP DATABASE IF EXISTS bankdb;
+-- CREATES THE BANK DATABASE
+create database if not exists bankdb;
 
+use bankdb;
 
-
+-- CREATES THE CUSTOMER TABLE
 DROP TABLE IF EXISTS `Customer` ;
 
 CREATE TABLE IF NOT EXISTS `Customer` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `FirstName` VARCHAR(45) NOT NULL,
   `LastName` VARCHAR(45) NOT NULL,
+  `Gender` VARCHAR(45) NOT NULL,
   `DOB` DATE NOT NULL,
   `RelationshipStatus` VARCHAR(45) NOT NULL,
   `PhoneNumber` VARCHAR(45) NOT NULL,
@@ -16,17 +21,15 @@ CREATE TABLE IF NOT EXISTS `Customer` (
   `Town_City` VARCHAR(45) NOT NULL,
   `CountryOfResidence` VARCHAR(45) NOT NULL,
   `PostalCode` VARCHAR(45) NOT NULL,
-  `OnlineBankingAcct_ID` INT NOT NULL,
   PRIMARY KEY (`ID`)
   )
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Customer_OnlineBankingAcct_idx` ON `Customer` (`OnlineBankingAcct_ID` ASC) VISIBLE;
 
 CREATE UNIQUE INDEX `ID_UNIQUE` ON `Customer` (`ID` ASC) VISIBLE;
 
 
-
+-- CREATES THE ACCOUNT TYPE TABLE FOR REFERENCES
 
 DROP TABLE IF EXISTS `AccountType` ;
 
@@ -38,7 +41,7 @@ ENGINE = InnoDB;
 
 
 
-
+-- CREATES THE ONLINE BANKING ACCOUNT TABLE
 
 DROP TABLE IF EXISTS `OnlineBankingAcct` ;
 
@@ -56,14 +59,14 @@ ENGINE = InnoDB;
 CREATE UNIQUE INDEX `Email_UNIQUE` ON `OnlineBankingAcct` (`Email` ASC) VISIBLE;
 
 
-
+-- CREATES THE ACCOUNTS TABLE
 
 
 DROP TABLE IF EXISTS `Accounts` ;
 
 CREATE TABLE IF NOT EXISTS `Accounts` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `OnlineBankingAcct_ID` INT NOT NULL,
+  `OnlineBankingAcct_ID` INT NULL,
   `Customer_ID` INT NOT NULL,
   PRIMARY KEY (`ID`),
   CONSTRAINT `fk_Accounts_OnlineBankingAcct1`
@@ -84,7 +87,7 @@ CREATE INDEX `fk_Accounts_Customer1_idx` ON `Accounts` (`Customer_ID` ASC) VISIB
 
 
 
-
+-- CREATES THE ACCOUNT TABLE
 
 DROP TABLE IF EXISTS `Account` ;
 
@@ -113,7 +116,7 @@ CREATE INDEX `fk_Account_Accounts1_idx` ON `Account` (`Accounts_ID` ASC) VISIBLE
 
 
 
-
+-- CREATES THE TRANSACTION TABLE
 
 DROP TABLE IF EXISTS `Transaction` ;
 
@@ -141,4 +144,61 @@ ENGINE = InnoDB;
 
 CREATE INDEX `fk_Transaction_Account1_idx` ON `Transaction` (`Account_AccountNumber` ASC) VISIBLE;
 
+
+-- INSERTS REQUIRED VALUES FOR THE ACCOUNT TYPE TABLE
+USE bankdb;
+INSERT INTO accounttype VALUES (1,'CHEQUING');
+INSERT INTO accounttype VALUES (2, 'SAVINGS');
+INSERT INTO accounttype VALUES (3, 'INVESTMENT');
+
+-- CREATES THE TRIGGER FOR CREATING FINANCIAL ACCOUNT ONCE A NEW CUSTOMER IS ADDED TO THE BANK DEFAULT IS CHEQUINGS ACCOUNT CREATION
+drop trigger if exists create_financial_account;
+
+delimiter //
+create trigger create_financial_account
+	after insert on Customer
+	for each row
+BEGIN
+
+	declare accountsid int;
+	declare customer_id int;
+	declare onlinebankid int;
+	declare account_number int;
+	
+	select ID into customer_id from Customer where ID = new.ID;
+	set accountsid = FLOOR(RAND() * (9999 - 1000 + 1)) + 1000;
+	set account_number = concat(accountsid,'',FLOOR(RAND() * (9999 - 1000 + 1)) + 1000);
+	
+	insert into Accounts (ID,OnlineBankingAcct_ID,Customer_ID) values (accountsid,Null,customer_id);
+	insert into Account values (account_number,0,True,1,accountsid);
+END //
+
+delimiter ;
+
+-- CREATES THE TRIGGER FOR CREATING ONLINE BANKING ACCOUNT ONCE A NEW ONLINE ACCOUNT INFORMATION IS INSERTED TO THE ONLINE BANK ACCOUNT TABLE
+
+drop trigger if exists create_online_account;
+
+delimiter //
+create trigger create_online_account
+	after insert on onlinebankingacct
+	for each row
+BEGIN
+
+	
+	declare customer_id int;
+	declare onlinebankid int;
+    declare accountsid int;
+	
+    select customerID into customer_id from onlinebankingacct where ID = new.ID;
+	select ID into accountsid from accounts where Customer_ID = customer_id;
+    select ID into onlinebankid from onlinebankingacct where ID = new.ID;
+	
+	
+	
+	update Accounts set OnlineBankingAcct_ID = onlinebankid where ID = accountsid;
+	
+END //
+
+delimiter ;
 
